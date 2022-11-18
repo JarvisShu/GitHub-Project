c03ac36fd90f9be2e54952094d1e29d2e1b48edc
jarvis <18159850853@163.com>
2022-11-18 17:36:35 +0800

    GitHub-Project:BubbleSort2

--- Algorithm-Sort/src/main/java/com/binary/bubblle/BubbleSort.java
+++ Algorithm-Sort/src/main/java/com/binary/bubblle/BubbleSort.java
@@ -1,8 +1,47 @@
-package com.binary.bubblle;/**
-*@ProjectName: GitHub-Project
-*@PackageName: com.binary.bubblle
-*@authoer: Jarvis Xu
-*@createDate: 2022/11/18  17:10
-*@description: 
-*/public class BubbleSort {
+package com.binary.bubblle;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * @ProjectName: GitHub-Project
+ * @PackageName: com.binary.bubblle
+ * @authoer: Jarvis Xu
+ * @createDate: 2022/11/18  17:10
+ * @description:
+ */
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(1000) + 1;
+        }
+        System.out.println(Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println(" ------------------ ");
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void bubbleSort(int[] array) {
+        if (array == null){
+            return;
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            boolean flag = false;
+            for (int j = 0; j < array.length - 1 - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[ j + 1];
+                    array[ j + 1] = array[j];
+                    array[j] = temp;
+                    flag = true;
+                }
+            }
+            if (!flag){
+                break;
+            }
+        }
+    }
+
+
 }
